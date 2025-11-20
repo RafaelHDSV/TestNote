e4a9241650ce01f57355fda12d1074240e998bfe
Rafael Vieira <rafaelvieira1720@gmail.com>
2025-11-20 09:14:17 -0300

    initial commit

---
+++ TestNote/TestNote/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace TestNote
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ TestNote/TestNote/GlobalXmlns.cs
@@ -0,0 +1,2 @@
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "TestNote")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "TestNote.Pages")]
